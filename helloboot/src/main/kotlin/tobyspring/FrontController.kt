0a25d99c3dc302fc5ac79bdf7148c6008020fae7
DolphaGo <adamdoha@naver.com>
2023-01-27 03:39:19 +0900

    Refactor: 스프링 컨테이너 사용

--- helloboot/src/main/kotlin/tobyspring/FrontController.kt
+++ helloboot/src/main/kotlin/tobyspring/FrontController.kt
@@ -5,17 +5,23 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
-import org.springframework.http.HttpHeaders
+import org.springframework.context.support.GenericApplicationContext
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import java.util.function.Supplier
 
 class FrontController
 
 fun main(args: Array<String>) {
+    val applicationContext = GenericApplicationContext()
+    // 스프링 컨테이너는 어떤 클래스의 메타정보를 넣어주는 방식으로 빈을 생성한다.
+    applicationContext.registerBean(HelloController::class.java, Supplier { HelloController() })
+    applicationContext.refresh() // 컨테이너 초기화
+
     val serverFactory = TomcatServletWebServerFactory()
     val webServer = serverFactory.getWebServer(ServletContextInitializer {
-        val helloController = HelloController()
+//        val helloController = HelloController()
 
         it.addServlet(
             "frontcontroller",
@@ -24,10 +30,13 @@ fun main(args: Array<String>) {
                     // 인증, 보안, 다국어, 공통 기능
                     if (req.requestURI.equals("/hello") && req.method == HttpMethod.GET.name()) {
                         val name = req.getParameter("name")
+
+                        val helloController = applicationContext.getBean(HelloController::class.java)
                         val result = helloController.hello(name) // 바인딩이라고 한다. 웹 요청으로 받은 것을 처리하는 오브젝트에서 사용할 수 있도록 하는 것.
 
-                        resp.status = HttpStatus.OK.value() // 서블릿에서 특별히 오류를 내지 않으면 200 OK가 리턴이 되긴한다. (Default 값임)
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
+//                        resp.status = HttpStatus.OK.value() // 서블릿에서 특별히 오류를 내지 않으면 200 OK가 리턴이 되긴한다. (Default 값임)
+//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE) // 이와 같은 방법보다 아래의 방법이 더 좋음
+                        resp.contentType = MediaType.TEXT_PLAIN_VALUE
                         resp.writer?.println(result)
                     } else if (req.requestURI.equals("/user")) {
                         resp.status = HttpStatus.ACCEPTED.value()
