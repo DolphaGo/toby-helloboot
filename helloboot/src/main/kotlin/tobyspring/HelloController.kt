445fd34d461bc55fceb4c6b6f0d3ff6807e75f21
DolphaGo <adamdoha@naver.com>
2023-02-01 04:09:03 +0900

    DI

--- helloboot/src/main/kotlin/tobyspring/HelloController.kt
+++ helloboot/src/main/kotlin/tobyspring/HelloController.kt
@@ -1,5 +1,8 @@
 package tobyspring
 
+// 스프링 컨테이너의 역할 : 메타 정보를 받아서 직접 오브젝트를 생성한다.
+// 게다가 Assembler의 역할을 한다. 오브젝트를 주입해주는 역할
+// HelloController가 HelloService를 볼 때, SimpleHelloService, ComplexHelloService 를 HelloService를 바라보도록 한다.
 class HelloController {
     fun hello(name: String): String {
         val service = SimpleHelloService()
