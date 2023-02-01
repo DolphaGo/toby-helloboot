d29c641fa7036c1991724991e4993bdc0925620f
DolphaGo <adamdoha@naver.com>
2023-02-02 03:52:49 +0900

    DispatcherServlet 으로 전환

--- helloboot/src/main/kotlin/tobyspring/FrontController.kt
+++ helloboot/src/main/kotlin/tobyspring/FrontController.kt
@@ -1,20 +1,15 @@
 package tobyspring
 
-import jakarta.servlet.http.HttpServlet
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
-import org.springframework.context.support.GenericApplicationContext
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
+import org.springframework.web.context.support.GenericWebApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
 import java.util.function.Supplier
 
 class FrontController
 
 fun main(args: Array<String>) {
-    val applicationContext = GenericApplicationContext()
+    val applicationContext = GenericWebApplicationContext()
     // 스프링 컨테이너는 어떤 클래스의 메타정보를 넣어주는 방식으로 빈을 생성한다.
     // 스프링 컨테이너는 싱글톤 방식으로 동작하기에 싱글톤 레지스트리라고도 한다.
     // 동일한 빈을 여러 서블릿 컨테이너에서 사용할 수가 있다.
@@ -24,35 +19,55 @@ fun main(args: Array<String>) {
 
     val serverFactory = TomcatServletWebServerFactory()
     val webServer = serverFactory.getWebServer(ServletContextInitializer {
-//        val helloController = HelloController()
-
         it.addServlet(
-            "frontcontroller",
-            object : HttpServlet() {
-                override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
-                    // 인증, 보안, 다국어, 공통 기능
-                    if (req.requestURI.equals("/hello") && req.method == HttpMethod.GET.name()) {
-                        val name = req.getParameter("name")
-
-                        val helloController = applicationContext.getBean(HelloController::class.java)
-                        val result = helloController.hello(name) // 바인딩이라고 한다. 웹 요청으로 받은 것을 처리하는 오브젝트에서 사용할 수 있도록 하는 것.
-
-//                        resp.status = HttpStatus.OK.value() // 서블릿에서 특별히 오류를 내지 않으면 200 OK가 리턴이 되긴한다. (Default 값임)
-//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE) // 이와 같은 방법보다 아래의 방법이 더 좋음
-                        resp.contentType = MediaType.TEXT_PLAIN_VALUE
-                        resp.writer?.println(result)
-                    } else if (req.requestURI.equals("/user")) {
-                        resp.status = HttpStatus.ACCEPTED.value()
-                    } else {
-                        resp.status = HttpStatus.NOT_FOUND.value()
-                    }
-                }
-            }
+            "dispatcherServlet",
+            DispatcherServlet(applicationContext) // DispatcherServlet은 WebApplicationContext를 이용합니다.
         ).addMapping("/*") // 모든 요청을 프론트 컨트롤러가 받는다. (중앙 집중화)
     })
 
     webServer.start()
 }
+
+//fun main(args: Array<String>) {
+//    val applicationContext = GenericApplicationContext()
+//    // 스프링 컨테이너는 어떤 클래스의 메타정보를 넣어주는 방식으로 빈을 생성한다.
+//    // 스프링 컨테이너는 싱글톤 방식으로 동작하기에 싱글톤 레지스트리라고도 한다.
+//    // 동일한 빈을 여러 서블릿 컨테이너에서 사용할 수가 있다.
+//    applicationContext.registerBean(HelloService::class.java, Supplier { SimpleHelloService() }) // 인터페이스가 아닌 클래스 타입으로 제공해야 한다.
+//    applicationContext.registerBean(HelloController::class.java, Supplier { HelloController(applicationContext.getBean(HelloService::class.java)) })
+//    applicationContext.refresh() // 컨테이너 초기화
+//
+//    val serverFactory = TomcatServletWebServerFactory()
+//    val webServer = serverFactory.getWebServer(ServletContextInitializer {
+////        val helloController = HelloController()
+//
+//        it.addServlet(
+//            "frontcontroller",
+//            object : HttpServlet() {
+//                override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
+//                    // 인증, 보안, 다국어, 공통 기능
+//                    if (req.requestURI.equals("/hello") && req.method == HttpMethod.GET.name()) {
+//                        val name = req.getParameter("name")
+//
+//                        val helloController = applicationContext.getBean(HelloController::class.java)
+//                        val result = helloController.hello(name) // 바인딩이라고 한다. 웹 요청으로 받은 것을 처리하는 오브젝트에서 사용할 수 있도록 하는 것.
+//
+////                        resp.status = HttpStatus.OK.value() // 서블릿에서 특별히 오류를 내지 않으면 200 OK가 리턴이 되긴한다. (Default 값임)
+////                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE) // 이와 같은 방법보다 아래의 방법이 더 좋음
+//                        resp.contentType = MediaType.TEXT_PLAIN_VALUE
+//                        resp.writer?.println(result)
+//                    } else if (req.requestURI.equals("/user")) {
+//                        resp.status = HttpStatus.ACCEPTED.value()
+//                    } else {
+//                        resp.status = HttpStatus.NOT_FOUND.value()
+//                    }
+//                }
+//            }
+//        ).addMapping("/*") // 모든 요청을 프론트 컨트롤러가 받는다. (중앙 집중화)
+//    })
+//
+//    webServer.start()
+//}
 
 /**
  * 위의 결과는 다음과 같다.
