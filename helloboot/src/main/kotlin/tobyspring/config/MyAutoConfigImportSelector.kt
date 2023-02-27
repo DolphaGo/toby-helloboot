169a020f51e0ae207f818d4922670d167073f174
DolphaGo <adamdoha@naver.com>
2023-02-28 02:10:04 +0900

    자동 구성 정보 파일 분리

--- helloboot/src/main/kotlin/tobyspring/config/MyAutoConfigImportSelector.kt
+++ helloboot/src/main/kotlin/tobyspring/config/MyAutoConfigImportSelector.kt
@@ -1,13 +1,24 @@
 package tobyspring.config
 
+import org.springframework.boot.context.annotation.ImportCandidates
 import org.springframework.context.annotation.DeferredImportSelector
 import org.springframework.core.type.AnnotationMetadata
 
-class MyAutoConfigImportSelector : DeferredImportSelector {
+class MyAutoConfigImportSelector(
+    private val classLoader: ClassLoader
+) : DeferredImportSelector {
+
     override fun selectImports(importingClassMetadata: AnnotationMetadata): Array<String> {
-        return arrayOf(
-            "tobyspring.config.autoconfig.DispatcherServletConfig",
-            "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        )
+        // ApplicationContextAware interface 구현 -> Container가 Bean 생성한 것 처럼
+        // classLoader 를 구현하는 방법으로 BeanClassLoaderAware interface 를 구현해서 BeanClassLoader 를 구현하는 방법도 있음
+
+        val autoConfigs = arrayListOf<String>()
+        val candidates: Iterable<String> = ImportCandidates.load(MyAutoConfiguration::class.java, classLoader)
+
+        // 다음과 같이 표현할 수 있지만, ㅎㅎ 읽기가 어려울 수도 있으니.
+//        return StreamSupport.stream(candidates.spliterator(), false).toArray { i -> arrayOfNulls<String>(i) }
+        candidates.forEach { autoConfigs.add(it) }
+
+        return autoConfigs.toArray(arrayOf())
     }
 }
---
+++ helloboot/src/main/kotlin/tobyspring/config/MyAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package tobyspring.config
+
+import org.springframework.context.annotation.Configuration
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
+@Configuration
+annotation class MyAutoConfiguration
