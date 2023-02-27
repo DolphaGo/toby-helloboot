91b649c3b103ccdb0d047a99fcfe9f067633f951
DolphaGo <adamdoha@naver.com>
2023-02-28 02:34:41 +0900

    자동 구성 애노테이션 적용

--- helloboot/src/main/kotlin/tobyspring/config/MyAutoConfiguration.kt
+++ helloboot/src/main/kotlin/tobyspring/config/MyAutoConfiguration.kt
@@ -4,5 +4,11 @@ import org.springframework.context.annotation.Configuration
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
-@Configuration
+@Configuration(proxyBeanMethods = false)
 annotation class MyAutoConfiguration
+
+
+/**
+ * proxyBeanMethods = false 는 무얼 의미할까
+ *
+ */
